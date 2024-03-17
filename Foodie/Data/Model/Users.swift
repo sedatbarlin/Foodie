0cee7f03b8469e5d747841234eb0fbbd77afb137
Sedat Barlin <barlinsedat@gmail.com>
2024-03-17 16:21:31 +0300

    Update Users.swift

--- Foodie/Data/Model/Users.swift
+++ Foodie/Data/Model/Users.swift
@@ -10,7 +10,7 @@ import Foundation
 final class Users { //Codable: JSON cevabını liste olarak alma (özel model)
     let email: String
     let password: String
-    
+     
     init(email: String, password: String) {
         self.email = email
         self.password = password
