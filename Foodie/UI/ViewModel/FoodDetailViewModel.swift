485e163fba2f0ce258b6f3c05de28a59d6b6dd5b
Sedat Barlin <barlinsedat@gmail.com>
2024-03-20 09:57:55 +0300

    Update FoodDetailViewModel.swift

--- Foodie/UI/ViewModel/FoodDetailViewModel.swift
+++ Foodie/UI/ViewModel/FoodDetailViewModel.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 import RxSwift
 
-//MARK: Detay sayfa görünümü için repo'dan çağırılacak addCart fonksiyonu  
+//MARK: Detay sayfa görünümü için repo'dan çağırılacak addCart fonksiyonu   
 
 final class FoodDetailViewModel{
     private let repository = FoodsDaoRepository()
