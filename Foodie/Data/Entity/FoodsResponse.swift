83899de9cc48ec2345ce58fc7a6fb9a3fcf5b59f
Sedat Barlin <barlinsedat@gmail.com>
2024-03-26 10:23:13 +0300

    Update FoodsResponse.swift

--- Foodie/Data/Entity/FoodsResponse.swift
+++ Foodie/Data/Entity/FoodsResponse.swift
@@ -5,7 +5,7 @@
 //  Created by Sedat on 15.12.2023.
 //
  
-import Foundation
+import Foundation 
   
 final class FoodsResponse: Codable{ //Codable: JSON cevabını liste olarak alma
     var yemekler: [Foods]?
