61e1759aa877f350f265d50d037af9da1492af0b
Sedat Barlin <barlinsedat@gmail.com>
2024-03-26 10:22:51 +0300

    Update Foods.swift

--- Foodie/Data/Model/Foods.swift
+++ Foodie/Data/Model/Foods.swift
@@ -5,7 +5,7 @@
 //  Created by Sedat on 15.12.2023.
 //
 
-import Foundation  
+import Foundation   
  
 final class Foods: Codable{ //Codable: JSON cevabını liste olarak alma (özel model)
     let yemek_id: String 
